50bf4f77ad53dd13c7434dd94779957d75e2ae55
karthik-karalgikar <91562537+karthik-karalgikar@users.noreply.github.com>
2023-12-07 01:37:35 +0530

    Update LC58lengthOfLastWord.py

--- Day_9_ArrayEasy/LC58lengthOfLastWord.py
+++ Day_9_ArrayEasy/LC58lengthOfLastWord.py
@@ -6,3 +6,23 @@ def lengthOfLastWord(s):
 s = "Hello World"
 result = lengthOfLastWord(s)
 print(result)
+
+'''
+TRACING:
+First I create a function called lengthOfLastWord with one parameter, 
+that is, 's' which is the input string
+Next, I use the split() function to convert the string into a list
+and I will store the value into a variable called 'wordList'
+For example, the string is "Hello World". 
+When I use the split function, the output will be ->
+["Hello", "World"]
+
+Next I will access the last element of the list using wordList[-1] 
+and store that value in a variable called lastWord.
+Here, the last element of the list wordList is "World"
+So, lastWord will have the value "World".
+
+Finally I will return the length of lastWord(length of the word
+"World" is 5), which is the expected
+output.
+'''

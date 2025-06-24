c491f53399421c87551478d9f0ef613250bacf0c
karthik-karalgikar <karalgikarkarthik@gmail.com>
2025-06-25 04:22:09 +0530

    LC 379 perfect square

---
+++ Day_19_LC/LC379PerfectSquare.py
@@ -0,0 +1,19 @@
+def isPerfectSquare(num):
+    if num < 2:
+        return True
+
+    left = 2
+    right = num // 2
+    while left <= right:
+        mid = (left + right) // 2
+        square = mid * mid
+        if square == num:
+            return True
+        elif square < num:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
+
+print(isPerfectSquare(num=16))

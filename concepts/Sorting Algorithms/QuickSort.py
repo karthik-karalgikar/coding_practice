e49a84ca53875334e3787e9937438722269578e1
karthik-karalgikar <karalgikarkarthik@gmail.com>
2025-09-01 16:19:41 +0530

    quicksort

--- concepts/Sorting Algorithms/QuickSort.py
+++ concepts/Sorting Algorithms/QuickSort.py
@@ -15,3 +15,42 @@ def QuickSort(nums):
     return QuickSort(left) + [pivot] + QuickSort(right)
 
 print(QuickSort(nums=[3, 1, 2, 4]))
+
+'''
+TRACING - 
+
+nums = [3, 1, 2, 4]
+
+pivot = nums[0]
+pivot = 3
+left = []
+right = []
+
+for i in range(1, len(nums)):
+    -> i = 1
+    if nums[i] <= pivot:
+    -> nums[1] <= 3 -> 1 <=3 -> true
+        left.append(nums[i]) -> nums[1]
+        left = [1]
+
+    -> i = 2
+    if nums[i] <= pivot:
+    -> nums[2] <= 3 -> 2 <= 3-> true
+        left.append(nums[i]) -> nums[2]
+        left = [1, 2]
+
+    -> i = 3
+    if nums[i] <= pivot:
+    -> nums[3] <= 3 -> 4 <= 3 <= false
+
+    else:
+        right.append(nums[i]) -> nums[3]
+        right = [4]
+
+        left = [1, 2]
+        right = [4]
+        pivot = 3
+
+        return QuickSort(left)
+
+'''

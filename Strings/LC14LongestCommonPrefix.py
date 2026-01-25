4cf28fce6c1477ae78b087e358c9a2307d926983
karthik-karalgikar <karalgikarkarthik@gmail.com>
2026-01-25 19:21:10 +0530

    LC 14

--- Strings/LC14LongestCommonPrefix.py
+++ Strings/LC14LongestCommonPrefix.py
@@ -13,3 +13,29 @@ def longestCommonPrefix(strs):
                 return ''.join(result)
             
     return ''.join(result)
+
+'''
+TRACING - 
+strs = ["flower","flow","flight"]
+strs.sort() -> ["flight", "flow", "flower"]
+first = strs[0] -> flight
+last = strs[-1] -> flower
+
+len(first) = 6
+len(last) = 6
+
+if last[i] == first[i]:
+    f == f -> true
+    result.append(last[i]) -> result = ['f']
+
+    l == l -> true
+    result.append(last[i]) -> result = ['f', 'l']
+
+    i == o -> false
+
+    else:
+        return ''.join(result) -> 'fl'
+'''
+            
+
+        
